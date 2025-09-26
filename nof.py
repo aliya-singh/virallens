0d4b31bf855d7aaa59a3ef14f0fab1e8b416917f
aliya-singh <aliyasingh51564@gmail.com>
2025-09-27 04:22:17 +0530

    nof.py

---
+++ nof.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = pd.read_csv("laptopData.csv")
+df = df.dropna(thresh=df.shape[1]//2)
+df = df.fillna(df.median(numeric_only=True))
+for col in df.select_dtypes(include='object').columns:
+    df[col] = df[col].fillna(df[col].mode()[0])
+    df[col] = df[col].astype("category").cat.codes
+X = df.drop("Price", axis=1)
+print("Number of features:", X.shape[1])
